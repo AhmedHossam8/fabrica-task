3f150a7ca7c2eba9f0c1f8a641681999c5cffb39
AhmedHossam8 <ahmedhossamemara8@gmail.com>
2026-04-03 23:44:54 +0200

    fixing  custom_last_visit_date updating logic

--- customer_visits/customer_visits/doctype/customer_visit_request/customer_visit_request.py
+++ customer_visits/customer_visits/doctype/customer_visit_request/customer_visit_request.py
@@ -1,11 +1,16 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 
 class CustomerVisitRequest(Document):
 	def validate(self):
 		self.validate_single_active_visit()
+
+	def on_update(self):
+		if self.has_value_changed("workflow_state"):
+			self.update_customer_last_visit_date()
 
 	def validate_single_active_visit(self):
 		"""Only one visit request per customer may be in Draft or Approved at a time."""
@@ -30,6 +35,21 @@ class CustomerVisitRequest(Document):
 				)
 			)
 
-	def on_update_after_submit(self):
-		if self.workflow_state == "Completed":
-			frappe.db.set_value("Customer", self.customer, "custom_last_visit_date", self.visit_date)
+	def update_customer_last_visit_date(self):
+		"""Update customer's last visit date when request is completed."""
+		if not (self.workflow_state == "Completed" and self.customer and self.visit_date):
+			return
+
+		current_last_visit = frappe.db.get_value(
+			"Customer",
+			self.customer,
+			"custom_last_visit_date"
+		)
+
+		if not current_last_visit or getdate(self.visit_date) > getdate(current_last_visit):
+			frappe.db.set_value(
+				"Customer",
+				self.customer,
+				"custom_last_visit_date",
+				self.visit_date
+			)
